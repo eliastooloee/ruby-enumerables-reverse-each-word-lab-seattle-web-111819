a894e8aca5e70666b063c89b5fdde777dc2595ef
Elias Tooloee <elitooloee@gmail.com>
2019-11-14 10:31:46 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,8 +1,10 @@
 def reverse_each_word(sentence)
+ # Using split to transform string into array so we can use enumerator.
  array=sentence.split
  new=[]
  array.collect do |sentence|
    new << sentence.reverse
  end
+ # Using join to make the reversed elements of the array into a string again.
  new.join(" ")
  end
